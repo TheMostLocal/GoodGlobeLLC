c9b18d441b7615972677b74d3be34e4fcb2c55d6
Collin McGough <123339962+CollinMcGough@users.noreply.github.com>
2024-06-26 13:48:07 -0400

    Add files via upload

---
+++ WorkbookAndSketch/contactus.php
@@ -0,0 +1,15 @@
+<html>
+	<center>
+	<head>
+		<title>Contact and Leave Feedback</title>
+	</head>
+	<center>
+<body>
+
+<style>
+	body {
+		border: 8px groove rgb(0, 0, 0);
+	}
+
+</style>
+<h1>Contact and Give Us Feedback!</h1>
---
+++ WorkbookAndSketch/findaservice.php
@@ -0,0 +1,17 @@
+<html>
+	<center>
+	<head>
+		<title>Find A Service</title>
+	</head>
+	<center>
+<body>
+
+<style>
+
+	body {
+		border: 8px groove rgb(0, 0, 0);
+	}
+
+</style>
+
+<h1>Find A Service</h1>
