a3257b2f9114cce946fc281ce0b424a07b101335
Collin McGough <123339962+CollinMcGough@users.noreply.github.com>
2024-06-26 15:40:02 -0400

    Add files via upload

---
+++ Draft2.php
@@ -0,0 +1,167 @@
+<!Doctype Html>
+<html>
+	<head>
+	<title>Draft2</title>
+</head>
+	<!-- reference https://www.javatpoint.com/how-to-make-a-navigation-bar-in-html  -->
+
+<style>
+	h1 {
+ 		border:5px solid;
+  		border-block-start-style: groove
+  		border-color: coral;
+	}
+
+	body{
+		border: 8px groove rgb(0, 0, 0);
+		margin: 5px 0px 0px 0px
+	}
+</style>
+
+
+
+<img src= "https://cdn.vectorstock.com/i/500p/80/80/lawn-mower-man-gardener-cartoon-vector-1068080.avif" width="180" height="205"/>
+<img src= "https://st.depositphotos.com/1743476/2446/i/450/depositphotos_24461927-stock-photo-portrait-of-happy-businesspeople.jpg" width="300" height="200"/>
+<img src= "https://media.istockphoto.com/id/1373430803/vector/home-renovation-workers-repairman-team-building-house-painting-electric-finishing-works.jpg?s=612x612&w=0&k=20&c=67KJTPd_hNTLZKzKyCwNcT1YjQFmsoRiwKXKk34hDw0=" width="250" height="200"/>
+<img src= "https://img.freepik.com/free-photo/low-angle-architects-shaking-hands_23-2148269341.jpg?w=1480&t=st=1717466478~exp=1717467078~hmac=d650e280454e1aedb7abfb7d33a8c6160022398002d03cedb821101a7ae3a8c9" width="280" height="200" />
+
+<center><h1>Welcome to WM!</h1></center>
+
+<center>
+	<h3>Find a Service or Company 
+	<form action="#" method="POST">
+		<input type="text" name=" " required>
+	</h3>
+	</form>
+</center>
+
+<br>
+
+<center><h3>myBusiness Portal Login:</h3>
+<div style="background-color:rgb(187, 0, 0) ; inline-size: 500px;">
+	<div>
+
+	<form action="#" method="POST">
+	<br>
+		<label>myBusiness username</label>
+		<input type="text" name="username" required>
+	</div>
+	<div>
+		<label>myBusiness password</label>
+		<input type="text" name="password" required>
+	</div>
+	<br>
+	<div>
+		<input type="submit" value="Login">
+	</div>
+	</form>
+</div>
+</center>
+
+<center>
+	<h4>Forgot username or password
+		<input type="submit" value="I...Forgot">
+	<div>Dont have myBusiness?
+		<input type="submit" value="Register"></h4></div>
+	<h6>What is myBusiness?
+		<div> How to Register</h6>
+	</div>
+</center>
+
+<br>
+
+<center><h3>myMouth Portal Login:</h3>
+<div style="background-color:rgb(187, 0, 0) ; inline-size: 500px;">
+	<div>
+	<div style="background-color:rgb(187, 0, 0) ; inline-size: 500px;">
+	<div>
+
+	<form action="#" method="POST">
+	<br>
+		<label>myMouth username</label>
+		<input type="text" name="username" required>
+	</div>
+	<div>
+		<label>myMouth password</label>
+		<input type="text" name="password" required>
+	</div>
+	<br>
+	<div>
+		<input type="submit" value="Login">
+	</div>
+	</form>
+</div>
+</center>
+
+<center>
+	<h4>Forgot username or password?
+		<input type="submit" value="I...Forgot">
+	<div>Dont have myMouth?
+		<input type="submit" value="Register"></h4></div>
+	<h6>What is myMouth?
+	<div>How to Register</h6>
+	</div>
+</center>
+</body>
+</html>
+<!Doctype Html>
+<Html>   
+<Head>    
+<Title>   
+Make a Navigation Bar
+</Title>
+<style type=text/css> 
+body 
+{
+block-size: 125vh;
+margin-block-start: 80px;
+padding: 30px;
+background-size: cover;
+font-family: sans-serif;
+}
+header {
+background-color: orange;
+position: fixed;
+inset-inline-start: 0;
+inset-inline-end: 0;
+inset-block-start: 5px;
+block-size: 30px;
+display: flex;
+align-items: center;
+box-shadow: 0 0 25px 0 black;
+}
+header * {
+display: inline;
+}
+header li {
+margin: 20px;
+}
+header li a {
+color: blue;
+text-decoration: none;
+}
+</style> 
+</Head>
+<Body> 
+<header>
+<nav>
+<ul>
+<li>
+<a href="#"> Home </a>
+</li>
+<li>
+<a href="#"> About </a>
+</li>
+<li>
+<a href="#"> Contact </a>
+</li>
+<li> <a href="#"> Terms of use </a>
+</li>
+<li>
+<a href="#"> Join Us </a>
+</li>
+</ul>
+</nav>
+</header>
+</Body> 
+</Html>
---
+++ words.php
@@ -0,0 +1,16 @@
+<html>
+	<left>
+	<head>
+		<title>Words</title>
+	</head>
+	<left>
+<body>
+
+<style>
+	
+	body {
+		border: 8px groove rgb(0, 0, 0);
+	}
+
+</style>
+<h1>Words</h1>
