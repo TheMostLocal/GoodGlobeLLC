b9413fd5fd875b7fd2c9fbabb640d773e63584c4
TheMostLocal <123339962+TheMostLocal@users.noreply.github.com>
2024-08-22 01:51:02 +0000

    new main with react

---
+++ index.js
@@ -0,0 +1,18 @@
+import {createRoot} from 'react-dom/client';
+
+// Clear the existing HTML content
+document.body.innerHTML = '<div id="app"></div>';
+
+// Render your React component instead
+const root = createRoot(document.getElementById('app'));
+root.render(<h1>Hello, world</h1>);
+
+function NavigationBar() {
+    // TODO: Actually implement a navigation bar
+    return <h1>Hello from React!</h1>;
+  }
+  
+  const domNode = document.getElementById('navigation');
+  const root = createRoot(domNode);
+  root.render(<NavigationBar />);
+  
